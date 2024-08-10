bf9be4d1ba682a1e44045fd2818592f8817467ef
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2024-08-10 14:43:41 -0300

    feat: add binary search

---
+++ binary-search.ts
@@ -0,0 +1,24 @@
+const binarySearch = (item: number, list: number[]) => {
+  let start = 0;
+  let end = list.length;
+
+  while (start <= end) {
+    const middle = Math.floor((start + end) / 2);
+
+    const candidate = list[middle];
+
+    if (candidate === item) {
+      return middle;
+    }
+
+    if (candidate > item) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
+
+  return null;
+};
+
+export { binarySearch };
